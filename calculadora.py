e08ec660d9ef2f5a13ec7dc331b353b304486936
Adriano Araujo <Adriano Araujo>
2018-09-04 21:40:49 -0300

    inicio

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+def soma(num1, num2):
+try:
+return float(num1) + float(num2)
+	except:
+return None
---
+++ test_calculadora.py
@@ -0,0 +1,13 @@
+import pytest
+from calculadora import soma
+
+
+ def test_etapa1 ():
+
+    	assert soma (1,1) == 2
+ 	assert soma (3,2) == 5
+	assert soma ('-2','-3') == -5
+	assert soma (5,5) == 10
+	assert soma('n','o')== None 
+	assert soma ('-1','y')== None
+	assert soma ('cinco', 'um') == false 
