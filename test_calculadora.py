64e45821c5486c8070fec77db59256a381b0e764
adrianohunter <42879630+adrianohunter@users.noreply.github.com>
2018-09-11 17:34:00 -0300

    sdg

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,9 +3,8 @@ from calculadora import soma
 
 
 def test_soma ():
-
-    	assert soma (1, 1) == 2
- 	assert soma (3, 2) == 5
+	assert soma (1, 1) == 2
+	assert soma (3, 2) == 5
 	assert soma ('-2', '-3') == -5
 	assert soma (5, 5) == 10
 	assert soma ('n', 'o') == None 
